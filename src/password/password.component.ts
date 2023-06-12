06bf4e8219a9d4aff4ff9aab9dc95e44e88dc767
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-12 12:26:57 +0300

    update: update logic of validation password

--- src/password/password.component.ts
+++ src/password/password.component.ts
@@ -6,18 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./password.component.scss']
 })
 export class PasswordStrengthComponent {
-  password: string = '';
-  strengthSection: number = -1;
+ password: string = '';
 
-  calculatePasswordStrength() {
-    const passwordLength = this.password.length;
-
-    if (passwordLength === 0 || passwordLength < 8) {
-      this.strengthSection = 0;
-    } else if (passwordLength >= 8 && passwordLength < 12) {
-      this.strengthSection = 1;
-    } else {
-      this.strengthSection = 2;
+  calculateStrength() {
+    if (!this.password.length) {
+      this.updateSections('gray', 'gray', 'gray');
+      return;
     }
+
+    if (this.password.length < 8) {
+      this.updateSections('red', 'red', 'red');
+      return;
+    }
+
+    if (this.isStrongPassword()) {
+      this.updateSections('green', 'green', 'green');
+    } else if (this.isMediumPassword()) {
+      this.updateSections('yellow', 'yellow', 'gray');
+    } else {
+      this.updateSections('red', 'gray', 'gray');
+    }
+  }
+
+  updateSections(section1: string, section2: string, section3: string) {
+    const sections = document.getElementsByClassName('section');
+    sections[0].className = `section ${section1}`;
+    sections[1].className = `section ${section2}`;
+    sections[2].className = `section ${section3}`;
+  }
+
+  hasLetters() {
+    return /[a-zA-Z]/.test(this.password);
+  }
+
+  hasDigits() {
+    return /[0-9]/.test(this.password);
+  }
+
+hasSymbols() {
+  return /[^\w]/.test(this.password);
+}
+
+
+  isStrongPassword() {
+    return this.hasLetters() && this.hasDigits() && this.hasSymbols();
+  }
+
+  isMediumPassword() {
+    const hasLetters = this.hasLetters();
+    const hasDigits = this.hasDigits();
+    const hasSymbols = this.hasSymbols();
+
+    return (
+      (hasLetters && hasDigits) ||
+      (hasLetters && hasSymbols) ||
+      (hasDigits && hasSymbols)
+    );
   }
 }
